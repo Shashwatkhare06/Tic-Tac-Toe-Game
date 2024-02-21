db71eb18d3b8ac628d850cf9eda307186d245ca8
Shashwatkhare06 <132262749+Shashwatkhare06@users.noreply.github.com>
2024-02-21 17:02:42 +0530

    Update app.js
    Added feature of colour changing background.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let resetBtn = document.querySelector("#reset");
 let newGameBtn = document.querySelector("#newGame");
 let msgContainer = document.querySelector(".msg-container");
 let msg = document.querySelector("#msg");
+var colourCode = document.getElementById("colour-code");
 
 let player0 = true;
 
@@ -22,6 +23,7 @@ const resetGame = () => {
     turn0 = true;
     enableBoxes();
     msgContainer.classList.add("hide");
+    getColor();
 
 }
 
@@ -33,15 +35,17 @@ boxes.forEach((btn) => {
         if (player0) {
             
              btn.innerText = "0";
+            colourCode.innerText = "X's Turn";
             player0 = false;
         }
         else {
         
             btn.innerText = "X";
+            colourCode.innerText = "0's Turn";
             player0 = true;
         }
         btn.disabled = true;
-
+        getColor();
         checkWinner();
 
     });
@@ -90,6 +94,27 @@ const checkWinner = () => {
 
 newGameBtn.addEventListener("click", resetGame);
 resetBtn.addEventListener("click", resetGame);
+
+
+const getColor = () => {
+
+    const randomNumber = Math.floor(Math.random() * 16777215);
+    const random = "#" + randomNumber.toString(16);
+    document.body.style.backgroundColor = random;
+    // navigator.clipboard.writeText("chance of");
+
+    // document.getElementById("colour-code").innerText = "X's chance.";
+}
+
+
+
+    document.getElementById("btn").addEventListener(
+
+        "click",
+        getColor
+    )
+
+
 
 
 /*Author - Pratham Chaudhary
